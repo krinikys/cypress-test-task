81656b9662e7b135c6506c5ab0d031fa9c40408c
andriiosiiuk-ux <andriiosiiuk@gmail.com>
2025-11-28 13:10:18 +0200

    solution

---
+++ cypress/constants/formValues.js
@@ -0,0 +1,16 @@
+export const Hobbies = {
+  Reading: 'Reading',
+  Sports: 'Sports',
+  Music: 'Music'
+};
+
+export const Gender = {
+  Male: 'Male',
+  Female: 'Female'
+};
+
+export const Time = {
+  Morning: 'Morning',
+  Noon: 'Noon',
+  Evening: 'Evening'
+};
